d5ae5e6c9a0f59176ef1f5262ea204ec1ec20700
Joseph <joseph@verifiedjoseph.com>
2022-04-18 17:07:26 +0100

    [global.js] Fix www regex

--- src/javascript/global.js
+++ src/javascript/global.js
@@ -17,7 +17,7 @@ var global = {
 	domainRegex: /^https?:\/\/([a-zA-Z0-9-.]{1,256})/,
 	subdomainRegex: /^https:\/\/(?!www\.)([a-zA-Z0-9-]+)\.[a-zA-Z0-9-]+\.[a-z]{2,20}/,
 	pathRegex: /^https?:\/\/(?:.+)(\/(?:.+)?)$/,
-	wwwRegex: /^https:\/\/www\./,
+	wwwRegex: /^https?:\/\/www\./,
 	protocolRegex: /^https?:\/\//,
 	ipRegex: /^https?:\/\/((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\:[0-9]{2,4})?)\//,
 
